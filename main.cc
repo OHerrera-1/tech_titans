3f7f1257451131bb18f07efe7e7f84def5e8b16f
OHerrera-1 <00084420@uca.edu.sv>
2024-10-06 14:35:49 -0600

    Creacion de estructura basica

--- main.cc
+++ main.cc
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <set>
 
+int main(int argc, char *argv[]) {
+  int opcion = 0;
+  do {
+    std::cout << "Ingrese la opcion que desea "
+                 "realziar\n1.Registrarte\n2.Acceder\n3.Observar\n4.Salir\nOpcion: ";
+    std::cin >> opcion;
+    switch (opcion) {
+      case 1:
+        /* Registrarse */
+        break;
 
-int main(int argc, char *argv[]){
+      case 2:
+        /* Ingresar */
+        break;
 
+      case 3:
+        /* Observar */
+        break;
+
+      default:
+        std::cout << "Opcion invalida";
+        break;
+    }
+  } while (opcion != 4);
 
   return 0;
 }
+
